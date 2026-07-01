b6f2d0f76a94533b4cd9390117714f0668cefa83
SAGAR RAJ <157152445+SAGAR-RAJ1@users.noreply.github.com>
2026-07-01 14:37:18 +0530

    Add solution for Odd Even Linked List

---
+++ solutions/medium/Odd Even Linked List.cpp
@@ -0,0 +1,27 @@
+// Title: Odd Even Linked List
+            // Difficulty: Medium
+            // Language: C++
+            // Link: https://leetcode.com/problems/odd-even-linked-list/
+
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        
+        ListNode * odd= head;
+       
+        while(even!=nullptr && even->next!=nullptr){
+
+class Solution {
+ */
+           odd->next=odd->next->next;
+        ListNode * even= head->next;
+        ListNode * evenhead= head->next;
+        if(head==nullptr || head->next==nullptr)return head;
+ * };
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+/**
