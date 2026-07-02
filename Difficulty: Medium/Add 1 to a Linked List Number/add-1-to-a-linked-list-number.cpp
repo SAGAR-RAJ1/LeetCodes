35ee9e2cb76effd856417819c8fe6540cec06346
SAGAR RAJ <157152445+SAGAR-RAJ1@users.noreply.github.com>
2026-07-02 10:11:51 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Add 1 to a Linked List Number/add-1-to-a-linked-list-number.cpp
+++ Difficulty: Medium/Add 1 to a Linked List Number/add-1-to-a-linked-list-number.cpp
@@ -1,43 +1,27 @@
 class Solution {
-public:
-    Node* reverseList(Node* head) {
-        Node* prev = nullptr;
-        Node* curr = head;
-
-        while (curr) {
-            Node* next = curr->next;
-            curr->next = prev;
-            prev = curr;
-            curr = next;
-        }
-
-        return prev;
-    }
-    Node* addOne(Node* head) {
-
-        head = reverseList(head);
-        Node * temp = head;
-        int carry = 1;
-        while(temp!=nullptr){
-            temp->data+=carry;
-            
-            if(temp->data<10){
-                carry=0;
-                break;
-            }else{
-                temp->data=0;
-                carry=1;
-            }
-            
-            temp=temp->next;
-        }
-       head= reverseList(head);
-        if(carry){
-            Node * dummy = new Node(1);
-            dummy->next = head;
-            head= dummy;
-        }
-        
-        return head;
-    }
+	public:
+	int solve(Node* head) {
+		
+		if(head==nullptr)return 1;
+		
+		int carry = solve(head->next);
+		
+		head->data+=carry;
+		
+		if(head->data<10)return 0;
+		head->data=0;
+		return 1;
+	}
+	Node* addOne(Node* head) {
+		
+		int carry = solve(head);
+		
+		if (carry) {
+			Node * dummy = new Node(1);
+			dummy->next = head;
+			head = dummy;
+		}
+		
+		return head;
+	}
 };
