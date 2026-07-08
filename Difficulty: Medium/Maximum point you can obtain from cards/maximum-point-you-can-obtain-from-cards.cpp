e23ec93e90106361878d245de2bdd8900dc39d49
SAGAR RAJ <157152445+SAGAR-RAJ1@users.noreply.github.com>
2026-07-08 13:00:04 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Maximum point you can obtain from cards/maximum-point-you-can-obtain-from-cards.cpp
@@ -0,0 +1,25 @@
+class Solution {
+  public:
+    int maxScore(vector<int>& arr, int k) {
+        // code here.
+        
+        int left = 0;
+        int right = 0;
+        int n = arr.size();
+        for(int i = 0 ; i<k;i++){
+             left+=arr[i];
+        }
+
+        int ans = left;
+
+        // if(k==n)return ans;
+        int rightindex=n-1;
+        for(int i = k-1;i>=0 ; i--){
+            left = left-arr[i];
+            right+= arr[rightindex];
+            rightindex--;
+            ans=max(ans,left+right);
+        }
+         return ans;
+    }
+};
