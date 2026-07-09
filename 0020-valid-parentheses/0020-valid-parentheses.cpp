1bcd788b86cd9ff5e0aae1440c23a3847c23da0a
SAGAR RAJ <157152445+SAGAR-RAJ1@users.noreply.github.com>
2026-07-09 18:10:56 +0530

    Time: 0 ms (100%), Space: 8.9 MB (38.15%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        
+        stack<char>st;
+
+        for(auto &a:s){
+
+            if(a=='('||a=='{'||a=='['){
+                st.push(a);
+            }else if(!st.empty() && ( a==')'&& st.top()=='('||a=='}'&& st.top()=='{'||a==']'&& st.top()=='[')){
+                st.pop();
+            }else{
+                return false;
+            }
+        }
+
+        return st.empty();
+    }
+};
