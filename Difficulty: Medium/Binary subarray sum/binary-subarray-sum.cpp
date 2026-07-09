15a5267d9ed5fc0660a4f907e84924c772fdf988
SAGAR RAJ <157152445+SAGAR-RAJ1@users.noreply.github.com>
2026-07-09 13:25:57 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Binary subarray sum/binary-subarray-sum.cpp
@@ -0,0 +1,25 @@
+class Solution {
+  public:
+  int help(vector<int>& nums, int goal) {
+        if(goal<0)return 0;
+        int r = 0,l=0,ans=0,sum=0;
+        int n = nums.size();
+        while(r<n){
+            sum+=nums[r];
+            while(sum>goal){
+                sum-=nums[l];
+                l++;
+            }  
+
+            ans+=(r-l+1);
+            r++;
+        }
+
+        return ans;
+    }
+    
+    int numberOfSubarrays(vector<int>& nums, int goal) {
+        // code here
+        return help(nums,goal)-help(nums,goal-1);
+    }
+};
