f67f648a218261f558ef85f506d85faa43586b61
SAGAR RAJ <157152445+SAGAR-RAJ1@users.noreply.github.com>
2026-08-02 16:50:01 +0530

    Time: 475 ms (12.23%), Space: 0B (100%) - LeetHub

--- 0601-human-traffic-of-stadium/0601-human-traffic-of-stadium.sql
+++ 0601-human-traffic-of-stadium/0601-human-traffic-of-stadium.sql
@@ -1,18 +1,12 @@
-WITH cte AS (
-    SELECT *,
-           id - ROW_NUMBER() OVER (ORDER BY id) AS grp
-    FROM Stadium
-    WHERE people >= 100
+with temp as (
+    select * , id - row_number() over(order by id) as rw
+    from stadium
+    where people>=100
 )
-
-SELECT id,
-       visit_date,
-       people
-FROM cte
-WHERE grp IN (
-    SELECT grp
-    FROM cte
-    GROUP BY grp
-    HAVING COUNT(*) >= 3
+select id,visit_date,people from temp
+where rw IN (
+    select rw from temp 
+    group by rw
+    having count(*)>=3
 )
-ORDER BY visit_date;
+order by visit_date
