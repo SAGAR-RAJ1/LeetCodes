25f9ed5a0e0c54936658bfe2b865aaad9e989463
SAGAR RAJ <157152445+SAGAR-RAJ1@users.noreply.github.com>
2026-08-07 17:44:44 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Add 1 to a Linked List Number/add-1-to-a-linked-list-number.cpp
+++ Difficulty: Medium/Add 1 to a Linked List Number/add-1-to-a-linked-list-number.cpp
@@ -1,27 +1,38 @@
+/* Structure of linked list Node
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int x) {
+        data = x;
+        next = nullptr;
+    }
+};
+*/
 class Solution {
-	public:
-	int solve(Node* head) {
-		
-		if(head==nullptr)return 1;
-		
-		int carry = solve(head->next);
-		
-		head->data+=carry;
-		
-		if(head->data<10)return 0;
-		head->data=0;
-		return 1;
-	}
-	Node* addOne(Node* head) {
-		
-		int carry = solve(head);
-		
-		if (carry) {
-			Node * dummy = new Node(1);
-			dummy->next = head;
-			head = dummy;
-		}
-		
-		return head;
-	}
+  public:
+    int help(Node * head){
+        if(!head){
+            return 1;
+        }
+        int carry = help(head->next);
+        head->data+=carry;
+        if(head->data<10)return 0;
+        head->data=0;
+        return 1;
+    }
+    Node* addOne(Node* head) {
+        // code here
+        int carry = help(head);
+        
+        if(carry){
+            Node* temp = new Node(1);
+            temp->next=head;
+            head=temp;
+            
+        }
+        return head;
+        
+    }
 };
