b7ef62ebc46908de431f9c15a7a3f946009be5a6
SAGAR RAJ <157152445+SAGAR-RAJ1@users.noreply.github.com>
2026-08-15 09:46:00 +0530

    Time: 7 ms (66.41%), Space: 17.8 MB (34.21%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,21 +1,34 @@
 class Solution {
-public:
-    int count = 0;
-    vector<vector<int>>dp;
-    void solve(vector<int>& nums, int target, int sum, int i) {
+public: 
+     vector<vector<int>>dp;
+    int solve(vector<int>& nums, int target, int i) {
 
         if (i < 0) {
-            if (sum == target) {
-                count++;
-            }
-            return;
+
+            if (target == 0)
+                return 1;
+            return 0;
         }
-        solve(nums, target, sum + nums[i], i - 1);
-        solve(nums, target, sum - nums[i], i - 1);
+        if(dp[i][target]!=-1)return dp[i][target];
+        int notpick = solve(nums, target, i - 1);
+        int pick = 0;
+        if (nums[i] <= target)
+            pick = solve(nums, target - nums[i], i - 1);
+
+        return dp[i][target]=pick + notpick;
     }
-    int findTargetSumWays(vector<int>& nums, int target) {
+    int findTargetSumWays(vector<int>& nums, int diff) {
+        int sum = 0;
         int n = nums.size();
-        solve(nums,target,0,n-1);
-        return count;
+
+        for (auto& a : nums)
+            sum += a;
+       
+        if (sum < abs(diff) || (sum + diff) % 2 == 1)
+            return 0;
+        int target = (sum + diff) / 2;
+        dp.assign(n,vector<int>(sum+1,-1));
+
+        return solve(nums, target, n - 1);
     }
 };
