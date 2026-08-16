288a591e8b52df6be51eb1b6c6c00ad86c5ee38d
SAGAR RAJ <157152445+SAGAR-RAJ1@users.noreply.github.com>
2026-08-16 16:32:17 +0530

    Time: 9 ms (48.13%), Space: 18.3 MB (41.94%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -3,22 +3,22 @@ public:
 
  vector<vector<vector<int>>> dp;
 int solve(vector<int>& prices,int i , int buy,int cap) {
-       int n = prices.size();
-
-       if(cap==0)return 0;
-       if(i==n)return 0;
-       if(dp[i][buy][cap]!=-1)return dp[i][buy][cap];
-
-       if(buy){
-        return dp[i][buy][cap]= max(solve(prices,i+1,1,cap),-prices[i]+solve(prices,i+1,0,cap));
-       }else{
+    //base cases
+    if(i==prices.size())return 0;
+    if(cap==0)return 0;
+    if(dp[i][buy][cap]!=-1)return dp[i][buy][cap];
+    // operations
+    if(buy){
+        return dp[i][buy][cap]=max(solve(prices,i+1,1,cap),-prices[i]+solve(prices,i+1,0,cap));
+    }else{
         return dp[i][buy][cap]=max(solve(prices,i+1,0,cap),prices[i]+solve(prices,i+1,1,cap-1));
-       }
+    }
+
 }
 
     int maxProfit(int k, vector<int>& prices) {
           int n = prices.size();
-            dp.assign(n, vector<vector<int>>(2, vector<int>(k+1, -1)));
+          dp.assign(n,vector<vector<int>>(2,vector<int>(k+1,-1)));
           return solve(prices,0,1,k);
     }
 };
