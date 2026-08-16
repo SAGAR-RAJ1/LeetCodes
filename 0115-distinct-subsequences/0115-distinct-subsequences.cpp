b76e19a8ad6070338e55ac8f9a5e6d36ce343d50
SAGAR RAJ <157152445+SAGAR-RAJ1@users.noreply.github.com>
2026-08-16 08:21:19 +0530

    Time: 27 ms (70.68%), Space: 44.4 MB (21.74%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,23 +1,37 @@
 class Solution {
-public: 
-    vector<vector<int>>dp;
-    int solve(string & s, string & t,int i , int j){
+public:
+    int numDistinct(string s, string t) {
 
-        ///base case
-         if(j<0)return 1;
-         if(i<0)return 0;
-         if(dp[i][j]!=-1)return dp[i][j];
-        if(s[i]==t[j]){
-            return dp[i][j]=solve(s,t,i-1,j-1)+solve(s,t,i-1,j);
-        }else{
-            return dp[i][j]=solve(s,t,i-1,j);
+        int m = s.size();
+        int n = t.size();
+
+        vector<vector<long long>> dp(
+            m + 1,
+            vector<long long>(n + 1, 0)
+        );
+
+        for(int i = 0; i <= m; i++) {
+            dp[i][0] = 1;
         }
-    }
-    int numDistinct(string & s, string & t) {
-         int m = s.size();
-         int n = t.size();
-         dp.assign(m,vector<int>(n,-1));
 
-         return solve(s,t,m-1,n-1);
+        for(int i = 1; i <= m; i++) {
+
+            for(int j = 1; j <= n; j++) {
+
+                if(s[i-1] == t[j-1]) {
+
+                    dp[i][j] =
+                        min((long long)INT_MAX,
+                            dp[i-1][j-1] + dp[i-1][j]);
+
+                }
+                else {
+
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+
+        return dp[m][n];
     }
 };
