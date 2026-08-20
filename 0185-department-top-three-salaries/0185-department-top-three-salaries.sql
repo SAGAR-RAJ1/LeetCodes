8efa2ffb361d3e1d16109f522671828f2a0b1b4d
SAGAR RAJ <157152445+SAGAR-RAJ1@users.noreply.github.com>
2026-08-20 21:41:47 +0530

    Time: 1209 ms (39.5%), Space: 0B (100%) - LeetHub

--- 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
+++ 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
@@ -1,9 +1,8 @@
-# Write your MySQL query statement below
-select Department,Employee,Salary from(
-select d.name as Department , e.name as Employee , e.Salary,
-dense_rank() over(partition by d.name order by e.salary desc) as dn
-from employee e
-join department d
-on d.id = e.departmentID
-)x
-where x.dn<4;
+select Department,Employee,Salary
+from
+(select d.name as Department,e.name as Employee,e.salary as Salary,
+dense_rank() over(partition by d.name order by salary desc) as dr
+from Employee e
+join Department d
+on e.departmentId = d.id)as x
+where x.dr<4
