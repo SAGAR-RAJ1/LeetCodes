442588fcaadb19b26074bd187598f6d615ee2a02
SAGAR RAJ <157152445+SAGAR-RAJ1@users.noreply.github.com>
2026-08-24 08:12:07 +0530

    Time: 55 ms (33.46%), Space: 32.4 MB (35.76%) - LeetHub

---
+++ 0887-super-egg-drop/0887-super-egg-drop.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<vector<int>>dp;
+    int solve(int e , int f){
+      
+      if(e==1)return f;
+
+      if(f==0 || f==1)return f;
+      if(dp[e][f]!=-1)return dp[e][f];
+       
+       int ans = INT_MAX;
+       int i =1 ; int j = f;
+
+       while(i<=j){
+        int mid = i + ( j - i)/2;
+
+        int breaks = solve(e-1,mid-1);
+        int nobreaks = solve(e,f-mid);
+
+        int temp = 1 + max(breaks,nobreaks);
+
+        if(breaks>nobreaks){
+            j=mid-1;
+        }else{
+            i=mid+1;
+        }
+        ans=min(ans,temp);
+       }
+
+      return dp[e][f]=ans;
+    }
+    int superEggDrop(int k, int n) {
+        dp.assign(k+1,vector<int>(n+1,-1));
+        return solve(k,n);
+    }
+};
