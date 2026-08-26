2c91b978ad45d98847a2ec8d5cd99786a4f502b1
SAGAR RAJ <157152445+SAGAR-RAJ1@users.noreply.github.com>
2026-08-26 05:42:15 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Merge two sorted linked lists/merge-two-sorted-linked-lists.cpp
+++ Difficulty: Medium/Merge two sorted linked lists/merge-two-sorted-linked-lists.cpp
@@ -15,37 +15,33 @@ class Solution {
   public:
     Node* sortedMerge(Node* head1, Node* head2) {
         // code here
-        Node * dummy = new Node (-1);
-        Node * curr = dummy;
-        
-        while(head1!=nullptr && head2!=nullptr){
+        Node * temp = new Node(-1);
+        Node* ans = temp;
+        while(head1 && head2){
             
             if(head1->data<=head2->data){
-                Node* temp = new Node(head1->data);
-                curr->next=temp;
-                curr=temp;
+                temp->next=head1;
+                temp=head1;
                 head1=head1->next;
+                
             }else{
-                Node* temp = new Node(head2->data);
-                curr->next=temp;
-                curr=temp;
+                temp->next=head2;
+                temp=head2;
                 head2=head2->next;
             }
+            
         }
         
-        while(head1!=nullptr){
-                Node* temp = new Node(head1->data);
-                curr->next=temp;
-                curr=temp;
-                head1=head1->next;
+        while(head1){
+            temp->next=head1;
+            temp=head1;
+            head1=head1->next;
         }
-        while(head2!=nullptr){
-                Node* temp = new Node(head2->data);
-                curr->next=temp;
-                curr=temp;
-                head2=head2->next;
+        while(head2){
+            temp->next=head2;
+            temp=head2;
+            head2=head2->next;
         }
-        curr->next=nullptr;
-        return dummy->next;
+        return ans->next;
     }
 };
