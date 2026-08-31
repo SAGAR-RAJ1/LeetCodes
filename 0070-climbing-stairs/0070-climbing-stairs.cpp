fa1e81f0513606197c74f58344df61441bf8f86d
SAGAR RAJ <157152445+SAGAR-RAJ1@users.noreply.github.com>
2026-08-31 15:48:03 +0530

    Time: 0 ms (100%), Space: 8.7 MB (20.91%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -3,10 +3,11 @@ public:
    vector<int>dp;
     int solve(int n){
         if(n==0)return 1;
-        if(n==1)return 1;
+       
          if(dp[n]!=-1)return dp[n];
         int l = solve(n-1);
-        int r = solve(n-2);
+        int r = 0;
+       if(n-2>=0) r=solve(n-2);
 
         return dp[n]=l+r;
 
