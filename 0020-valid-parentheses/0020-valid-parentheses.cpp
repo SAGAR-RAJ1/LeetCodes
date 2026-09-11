51e109460ba4c15f2187100b1c89a557c62853c9
SAGAR RAJ <157152445+SAGAR-RAJ1@users.noreply.github.com>
2026-09-11 18:49:09 +0530

    Time: 0 ms (100%), Space: 8.9 MB (67.2%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,20 +1,28 @@
 class Solution {
 public:
     bool isValid(string s) {
-        
+
         stack<char>st;
-
-        for(auto &a:s){
-
-            if(a=='('||a=='{'||a=='['){
-                st.push(a);
-            }else if(!st.empty() && ( a==')'&& st.top()=='('||a=='}'&& st.top()=='{'||a==']'&& st.top()=='[')){
-                st.pop();
+        for( auto &ch:s){
+            if(ch=='{' || ch=='[' || ch=='('){
+                st.push(ch);
             }else{
-                return false;
+                if(st.empty())return false;
+                if(ch=='}' && st.top()=='{'){
+                    st.pop();
+                }else if(ch==']' && st.top()=='['){
+                    st.pop();
+                }else if(ch==')' && st.top()=='('){
+                    st.pop();
+                }else{
+                    return false;
+                }
             }
         }
 
-        return st.empty();
+        if(!st.empty())return false;
+
+        return true;
+        
     }
 };
