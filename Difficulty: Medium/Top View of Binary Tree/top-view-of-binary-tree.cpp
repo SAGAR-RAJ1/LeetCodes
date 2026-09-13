b0cbdf5da5c2134e0e59a11a96ea28302b9e36e6
SAGAR RAJ <157152445+SAGAR-RAJ1@users.noreply.github.com>
2026-09-13 10:42:19 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Top View of Binary Tree/top-view-of-binary-tree.cpp
+++ Difficulty: Medium/Top View of Binary Tree/top-view-of-binary-tree.cpp
@@ -17,31 +17,27 @@ class Solution {
   public:
     vector<int> topView(Node *root) {
         // code here
-        vector<int>ans;
-        map<int,int>m;
-        
-        queue<pair<Node *,int>>q;
-        q.push({root,0});
-        while(!q.empty()){
-            
-            auto node = q.front().first;
-            auto level = q.front().second;
-            q.pop();
-            
-            if(m.find(level)==m.end()){
-                m[level]=node->data;
-            }
-            
-            if(node->left)q.push({node->left,level-1});
-            if(node->right)q.push({node->right,level+1});
-
-            
-        }
-        
-        for(auto &a:m){
-            ans.push_back(a.second);
-        }
-        
-        return ans;
+       //  verticle  node 
+       map<int,int>m;
+       queue<pair<int,Node*>>q;
+       q.push({0,root});
+       
+       while(!q.empty()){
+           int v = q.front().first;
+           auto node = q.front().second;
+           q.pop();
+           
+           if(m.find(v)==m.end()){
+               m[v]=node->data;
+           }
+           
+           if(node->left)q.push({v-1,node->left});
+           if(node->right)q.push({v+1,node->right});
+       }
+       vector<int>ans;
+       for( auto &a:m){
+           ans.push_back(a.second);
+       }
+       return ans;
     }
 };
