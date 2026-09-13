e6cbc2185b99cea28b1d78a7a25157a2f093d29b
SAGAR RAJ <157152445+SAGAR-RAJ1@users.noreply.github.com>
2026-09-13 11:44:00 +0530

    Time: 4 ms (65.78%), Space: 15.5 MB (48.76%) - LeetHub

--- 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.cpp
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.cpp
@@ -9,56 +9,46 @@
  */
 class Solution {
 public:
-    unordered_map<TreeNode*,TreeNode*>m;
+    unordered_map<TreeNode* , TreeNode*>m;
+
     void parent(TreeNode * root){
-        
-        if(root==nullptr)return;
 
-        if(root->left){
-            m[root->left]=root;
-        }
-        if(root->right){
-            m[root->right]=root;
-        }
-
+        if(!root)return ;
+        if(root->left)m[root->left]=root;
+        if(root->right)m[root->right]=root;
         parent(root->left);
         parent(root->right);
-
     }
+
     vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
-        vector<int>ans;
         
+        vector<int>ans;
+        unordered_map<TreeNode*,int>visited;
         parent(root);
-        queue<TreeNode *>q;
-        q.push(target);
-        unordered_map<TreeNode*,bool>visited;
-        visited[target]=true;
-
+        queue<pair<TreeNode*,int>>q;
+        q.push({target,0});
+        visited[target]=1;
         while(!q.empty()){
+            auto node = q.front().first;
+            int dist=q.front().second;
+            q.pop();
+            
+            if(dist==k)ans.push_back(node->val);
+            if(dist>k)continue;
 
-            int s =q.size();
-             
-            for(int i = 0 ; i<s;i++){
-                 
-                 auto node = q.front();
-                 q.pop();
-                 if(k==0)ans.push_back(node->val);
-                 if(node->left && !visited[node->left]){
-                    q.push(node->left);visited[node->left]=true;
-                    }
-                 if(node->right && !visited[node->right]){
-                    q.push(node->right);visited[node->right]=true;
-                    }
-                 if(m[node] &&  !visited[m[node]]){
-                    q.push(m[node]);visited[m[node]]=true;
-                 }
-
+            if(node->left && visited[node->left]!=1){
+                q.push({node->left,dist+1});
+                visited[node->left]=1;
             }
-            k--;
-
+            if(node->right && visited[node->right]!=1){
+                q.push({node->right,dist+1});
+                visited[node->right]=1;
+            }
+            if(m[node] && visited[m[node]]!=1){
+                q.push({m[node],dist+1});
+                visited[m[node]]=1;
+            }
         }
-
         return ans;
-
     }
 };
